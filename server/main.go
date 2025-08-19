9aefe3b08da23f22f77b2c682c8b3dc193a022b0
kiddo9 <dkido913@gmail.com>
2025-08-19 11:06:31 +0100

    done with cionflict

--- server/main.go
+++ server/main.go
@@ -2,16 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net"
+	"os"
 
+	"github.com/joho/godotenv"
 	"github.com/kiddo9/SMS-MAIL-SERVER/handlers"
 	pb "github.com/kiddo9/SMS-MAIL-SERVER/message/proto"
 	"github.com/kiddo9/SMS-MAIL-SERVER/middleware"
 	"google.golang.org/grpc"
 )
 
+func init(){
+	godotenv.Load(".env")
+}
+
 
 func main() {
 	grpcServer := grpc.NewServer(middleware.Interceptors(middleware.RecaptchaMiddleware, middleware.AuthMiddleware))
+	port := os.Getenv("PORT")
+
+	lis, err := net.Listen("tcp",":"+port)
 
 	runningMessage := fmt.Sprintf("Server is running on port %s", port)
 	fmt.Println(runningMessage)
