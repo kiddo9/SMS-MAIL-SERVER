ea78f2d3c84dee733e9984dc54677673a4899e7f
kiddo9 <dkido913@gmail.com>
2025-08-25 15:30:51 +0100

    updated

--- server/handlers/BatchUploadHandler.go
+++ server/handlers/BatchUploadHandler.go
@@ -3,10 +3,13 @@ package handlers
 import (
 	"bytes"
 	"context"
-	"fmt"
+	"encoding/json"
 	"io"
+	"os"
 
+	"github.com/kiddo9/SMS-MAIL-SERVER/config"
 	pb "github.com/kiddo9/SMS-MAIL-SERVER/message/proto"
+	"github.com/kiddo9/SMS-MAIL-SERVER/structures"
 	"github.com/xuri/excelize/v2"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -14,6 +17,31 @@ import (
 
 type FileUploadStruct struct {
 	pb.UnimplementedFileUploadServicesServer
+}
+
+var datas []byte
+var body []structures.AdminStructs
+var Admin structures.AdminStructs
+
+func init() {
+	_, err := os.Open(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	datas, err = os.ReadFile(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = json.Unmarshal(datas, &body)
+	if err != nil {
+		panic(err)
+	}
+
+	body[0] = Admin
 }
 
 func (f *FileUploadStruct) FileUpload(ctx context.Context, req *pb.FileUploadRequest)(*pb.FileUploadResponse, error){
@@ -41,7 +69,16 @@ func (f *FileUploadStruct) FileUpload(ctx context.Context, req *pb.FileUploadReq
 
 		for _, row := range result[sheet] {
 			name := row[0]
-			fmt.Printf("names : %v",name)
+			pendingPrice := row[1]
+			course := row[2]
+			data := row[3]
+			email := row[4]
+			
+			_, err = config.BulkEmail(name, pendingPrice, course, data, Admin.Email, Admin.Phone, email)
+
+			if err != nil {
+				return nil, status.Errorf(codes.Unknown, "unable to complete bulk email and sms")
+			}
 		}
 	}
 
